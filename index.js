c34805ead1f496ed31a0a3c4083f427038f5b97b
maoforero <maforeror@gmail.com>
2021-12-23 17:30:51 -0500

    query de creacion de db

--- index.js
+++ index.js
@@ -19,6 +19,29 @@ const db = db_obj.client;
 app.use(cors(config.cors));
 app.use("/productos", require('./routes/routerPro'))
 
+
+(async () => {
+    try{
+        let data = [
+            {
+                title: "Iphone 13",
+                price: "900",
+                img:"https://pngimg.com/uploads/iphone_13/iphone_13_PNG29.png"
+            }
+        ]
+
+        //create
+        await db.schema.createTable("productos", table => {
+            table.increments("id").primary(),
+            table.string("title"),
+            table.int("price"),
+            table.string("img")
+        })
+    }catch(error){
+        console.log(error)
+    }
+})
+
 //routes home
 
 app.get("/", (req, res) => {
